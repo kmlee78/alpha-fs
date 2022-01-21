b746431a6eb1477905225c77a93d4907c83fa7b8
kmlee78 <jaoming787@gmail.com>
2022-01-21 15:10:24 +0900

    refactor: remake datareader

--- alphafs/config.py
+++ alphafs/config.py
@@ -1,5 +1,27 @@
+# directory
+TEMP = "temp"
 DATA_DIR = "raw_data"
 OPERATION_DIR = "operation"
 OLD_DATA_DIR = "old_data"
+INDICATORS_DIR = "indicators"
+LATEST_INDICATOR = "latest"
 
+# data
 FS_DB = "fs"
+
+# data fields
+RECEPT_NO = "rcept_no"
+CORP_CODE = "corp_code"
+SJ_DIV = "sj_div"
+ACCOUNT_ID = "account_id"
+ACCOUNT_NM = "account_nm"
+
+# sj_div
+BS = "BS"
+CIS = "CIS"
+IS = "IS"
+SCE = "SCE"
+CF = "CF"
+
+# account_id
+NULL = "-표준계정코드 미사용-"
--- alphafs/datareader.py
+++ alphafs/datareader.py
@@ -1,34 +1,46 @@
 import pandas as pd
 import sqlalchemy as sa
 
-from alphafs.config import DATA_DIR, FS_DB, OPERATION_DIR
+from alphafs.config import (
+    ACCOUNT_ID,
+    ACCOUNT_NM,
+    CORP_CODE,
+    DATA_DIR,
+    FS_DB,
+    IS,
+    OPERATION_DIR,
+    RECEPT_NO,
+    SCE,
+    SJ_DIV,
+)
 from alphafs.log import main_logger
 from alphafs.messages import LOADING, PROCESS_DONE
 
-REQUIRED_FIELDS = ["rcept_no", "corp_code", "sj_div", "account_id", "account_nm"]
+REQUIRED_FIELDS = [
+    RECEPT_NO,
+    CORP_CODE,
+    SJ_DIV,
+    ACCOUNT_ID,
+    ACCOUNT_NM,
+]
+data_path = f"{DATA_DIR}/{OPERATION_DIR}/{FS_DB}.db"
+engine = sa.create_engine(f"sqlite:///{data_path}")
 
 
-class DataBaseReader:
-    def __init__(self):
-        self.data_path = f"{DATA_DIR}/{OPERATION_DIR}/{FS_DB}.db"
-
-    def read_database(self) -> pd.DataFrame:
-        engine = sa.create_engine(f"sqlite:///{self.data_path}")
-        df = pd.read_sql(f"SELECT * FROM {FS_DB}", engine)
-        return df
+def _load_data() -> pd.DataFrame:
+    df = pd.read_sql(f"SELECT * FROM {FS_DB}", engine)
+    return df
 
 
-class DataProvider:
-    def __init__(self):
-        self.datareader = DataBaseReader()
+def _process_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = df[REQUIRED_FIELDS]
+    df = df[(df[SJ_DIV] != SCE) & (df[SJ_DIV] != IS)]
+    return df
 
-    def load_data(self) -> pd.DataFrame:
-        main_logger.info(LOADING)
-        df = self.datareader.read_database()
-        main_logger.info(PROCESS_DONE)
-        return df
 
-    def fetch_data(self) -> pd.DataFrame:
-        df = self.load_data()
-        data = df[REQUIRED_FIELDS]
-        return data
+def fetch_data() -> pd.DataFrame:
+    main_logger.info(LOADING)
+    df = _load_data()
+    data = _process_data(df)
+    main_logger.info(PROCESS_DONE)
+    return data
