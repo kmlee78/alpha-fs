f87e3f6f88492028bd8cc61d684913b581d6a153
kmlee78 <jaoming787@gmail.com>
2022-03-11 18:02:06 +0900

    fix: fix directory reorganization logic

--- alphafs/postprocess.py
+++ alphafs/postprocess.py
@@ -3,14 +3,26 @@ import os
 from alphafs.config import DATA_DIR, HISTORY, INDICATORS_DIR, LATEST_INDICATOR
 
 
-def name_version(directory: str) -> str:
+def _is_version_folder(el):
+    if el.startswith("v"):
+        return el
+    else:
+        return
+
+
+def get_latest_version(directory: str) -> str:
     directory_components = os.listdir(directory)
-    directory_components.remove(LATEST_INDICATOR)
+    directory_components = list(filter(_is_version_folder, directory_components))
     version_numbers = [int(v[1:]) for v in directory_components]
     version_numbers.sort()
+    version_no = version_numbers[-1]
+    return "v" + str(version_no)
+
+
+def name_version(directory: str) -> str:
     try:
-        version_no = version_numbers[-1] + 1
-        return "v" + str(version_no)
+        version = get_latest_version(directory)
+        return "v" + str(int(version[1:]) + 1)
     except IndexError:
         return "v1"
 
